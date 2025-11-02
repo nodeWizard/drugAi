8b5cff7481fbf7c7f7318f66f9753343374740b2
nodeWizard <rawen@factory619.com>
2025-11-02 16:30:56 +0100

    update navbar color

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ function Navbar() {
   const isActive = (path: string) => location.pathname === path
 
   return (
-    <nav className="bg-[#1b253b] backdrop-blur-md shadow-md fixed top-0 left-0 right-0 z-50">
+    <nav className="bg-transparent backdrop-blur-md shadow-md fixed top-0 left-0 right-0 z-50">
       <div className="container mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex items-center justify-between h-16">
           {/* Logo */}
